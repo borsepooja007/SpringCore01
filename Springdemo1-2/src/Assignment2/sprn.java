9b6a7104c5a3e0d94330ac849e3f2bf21df5e145
borsepooja007 <59304709+borsepooja007@users.noreply.github.com>
2021-05-14 22:26:13 +0530

    Update sprn.java

--- Springdemo1-2/src/Assignment2/sprn.java
+++ Springdemo1-2/src/Assignment2/sprn.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations="/applicationContext.xml")
-public class Navin23 {
+public class sprn {
 
 	@Autowired
 	Questions ques;
